f030f6383f095182b19ce1eb17e1f93feea88fba
ryoyakawai <ryoya.kawai@gmail.com>
2015-11-11 02:19:45 +0900

    Import Codelab of "Moco for LUFA"

---
+++ arduino-mocolufa/sw.js
@@ -0,0 +1,49 @@
+/* sw.js */
+var CACHE_NAME = 'codelabs-mocolufa-cache';
+var urlsToCache = [
+    './images/arduino-mocolufa-144x144.png',
+    './step.ja-jp.html',
+    './index.html',
+    './index.html?ja-jp',
+    './manifest.json'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+      caches.open(CACHE_NAME)
+          .then(function(cache) {
+              //console.log('Opened cache', cache);
+              return cache.addAll(urlsToCache);
+          })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+      caches.match(event.request)
+          .then(function(response) {
+              if (response) {
+                  //console.log("[return cache] ", (response.url).split("/").pop());
+                  return response;
+              }
+              var fetchRequest = event.request.clone();
+
+              return fetch(fetchRequest)
+                  .then(function(response) {
+                      if (!response || response.status !== 200 || response.type !== 'basic') {
+                          return response;
+                      }
+                      
+                      var responseToCache = response.clone();
+
+                      caches.open(CACHE_NAME)
+                          .then(function(cache) {
+                              cache.put(event.request, responseToCache);
+                          });
+                      
+                      return response;
+                  });
+          })
+  );
+});
+
