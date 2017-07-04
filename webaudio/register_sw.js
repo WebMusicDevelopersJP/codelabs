4b01110f3dd7dd2cfdc4a353867e379cfd4eabf3
ryoyakawai <ryoya.kawai@gmail.com>
2017-07-04 12:25:20 -0700

    Sync to published version

--- webaudio/register_sw.js
+++ webaudio/register_sw.js
@@ -1,5 +1,12 @@
 window.addEventListener('load', function() {
   if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('./sw.js');
+      navigator.serviceWorker
+          .register('./sw.js')
+          .then( registration => {
+              console.log('ServiceWorker registration successful with scope: ', registration.scope);
+          })
+          .catch( err => {
+              console.log('ServiceWorker registration failed: ', err);
+          });
     }
 });
--- webaudio/sw.js
+++ webaudio/sw.js
@@ -1,1 +1,72 @@
 /* sw.js */
+
+const VERSION='v1.0.1';
+const CACHE_NAME = 'codelabs-webaduio-cache'+'_'+VERSION;
+const ASSETS = [
+    './images/webaudio-js-144x144.png',
+    './images/webmidi-js-144x144.png',
+    './step.en-us.html',
+    './step.ja-jp.html',
+    './step.zh-cn.html',
+    './index.html',
+    './index.html?en-us',
+    './index.html?ja-jp',
+    './index.html?zh-cn',
+    './manifest.json',
+    './contents/snare.wav',
+    './contents/ir/s1_r1_bd.wav',
+    './contents/loop.wav'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+      caches.open(CACHE_NAME)
+          .then( cache => {
+              return cache.addAll(ASSETS);
+          })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+      caches.match(event.request)
+          .then(response => {
+              if (response) {
+                  return response;
+              }
+              var fetchRequest = event.request.clone();
+
+              return fetch(fetchRequest)
+                  .then( response => {
+                      if (!response || response.status !== 200 || response.type !== 'basic') {
+                          return response;
+                      }
+                      
+                      var responseToCache = response.clone();
+
+                      caches.open(CACHE_NAME)
+                          .then( cache => {
+                              cache.put(event.request, responseToCache);
+                          });
+                      
+                      return response;
+                  });
+          })
+  );
+});
+
+self.addEventListener('activate', event => {
+    const cacheWhiteList=[];
+    cacheWhiteList.push(CACHE_NAME);
+    event.waitUntil(
+        caches.keys().then( cacheNames => {
+            return Promise.all(
+                cacheNames.map( cacheName => {
+                    if(cacheWhiteList.indexOf(cacheName)===-1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
